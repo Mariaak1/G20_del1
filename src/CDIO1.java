8d8d58a6284c6a5fb3b22cf2f5bac734a1b82a05
Mariaak1 <113905587+Mariaak1@users.noreply.github.com>
2022-09-19 16:46:34 +0200

    2 terninger

--- src/CDIO1.java
+++ src/CDIO1.java
@@ -1,15 +1,25 @@
 import java.util.Random;
+import java.lang.Math;
 
 public class CDIO1 {
 
     public static void main(String[] args) {
-        int Dice1;
-        int Dice2;
+        int Dice1 = 1+ (int) (Math.random() * 6);
+        int Dice2 = 1+(int) (Math.random() *6);
 
+        System.out.println(Dice1);
+        System.out.println(Dice2);
+
+
+
+
+
+
+
+        }
 
 
 
 
 
     }
-}
