2e334776e03494ad3c32bfd76c06101282782f26
Mariaak1 <113905587+Mariaak1@users.noreply.github.com>
2022-09-22 20:18:08 +0200

    anden metode for dice

---
+++ src/nymetodeterning.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.lang.Math;
+import java.util.Scanner;
+
+
+public class nymetodeterning {
+
+    public static void main(String[] args) {
+        int Dice1 = 1 + (int) (Math.random() * 6);
+        int Dice2 = 1 + (int) (Math.random() * 6);
+
+        System.out.println(Dice1);
+        System.out.println(Dice2);
+
+        void startNySpil
+            String S1navn;
+            String S2navn;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Skriv spiller1 navn");
+            S1navn = sc.nextLine();
+
+
+        }
+    }
+
+
+}
