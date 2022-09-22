4dc21410b8da90c40098acc83703630b60d38fa5
Mariaak1 <113905587+Mariaak1@users.noreply.github.com>
2022-09-22 15:48:05 +0200

    første omgang

--- src/CDIO1.java
+++ src/CDIO1.java
@@ -1,25 +1,42 @@
 import java.util.Random;
-import java.lang.Math;
+import java.lang.Math.*;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CDIO1 {
 
-    public static void main(String[] args) {
-        int Dice1 = 1+ (int) (Math.random() * 6);
-        int Dice2 = 1+(int) (Math.random() *6);
+    public static void main(String[] args){
+    int sum1=0, sum2=0;
+    int player, playerCount= 1;
+    while (true){
+        player=playerCount  % 2==0?2:1;
+        int sum= player==1 ? sum1:sum2;
+        int dice1P1=ThreadLocalRandom.current().nextInt(1,7);
+        int dice2P1=ThreadLocalRandom.current().nextInt(1,7);
+        sum=sum+dice1P1+dice2P1;
+        if(player==1) {
+            sum1 = sum;
+        }else {
+            sum2 = sum;
+        }
+        System.out.println("Player"+player+"rolls a"+dice2P1);
+        System.out.println("Player"+player+ "now has "+sum);
+        if(sum>=40) {
+            System.out.println("Player" + player + "Wins with total of" + sum);
+            break;
 
-        System.out.println(Dice1);
-        System.out.println(Dice2);
+        }else if (dice1P1==dice2P1) {
 
-
-
-
-
-
+            System.out.println("Player" + player + "gets to roll again");
+            continue;
+        }else {
+            playerCount++;
 
         }
-
-
+    }
 
 
 
     }
+}
+
